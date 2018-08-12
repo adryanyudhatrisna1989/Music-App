150f6bf5a24e1e739204b94991426bf0a6424658
adryanyudhatrisna1989 <ayudhatrisna1989@gmail.com>
2018-08-12 21:34:06 +0700

    Initial commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -2,15 +2,29 @@
 //  ViewController.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 27/01/2016.
-//  Copyright © 2016 London App Brewery. All rights reserved.
+//  Created by Adryan Yudhatrisna.
+//  Copyright © 2018
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var audioPlayer: AVAudioPlayer!
+    
+    var selectedSoundFileName: String = ""
+    
+    let soundArray = [
+        "note1",
+        "note2",
+        "note3",
+        "note4",
+        "note5",
+        "note6",
+        "note7"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,12 +32,24 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        print(selectedSoundFileName)
         
-        
-        
+        playSound()
     }
     
-  
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
 
 }
 
